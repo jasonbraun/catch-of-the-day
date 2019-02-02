35df82ff7f05119dcba460dce3cc07f258710182
Jason Braun <jason@braundesign.co>
2019-02-02 00:10:42 -0800

    Finish Lesson 12

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from
-  'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import StorePicker from "./StorePicker";
 import App from "./App";
 import NotFound from "./NotFound";
@@ -13,6 +12,6 @@ const Router = () => (
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
-)
+);
 
 export default Router;
